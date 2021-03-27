53adf0bdf02aeba2cf02a487446600a01c98f6ae
Liam Craft <crafty0410@gmail.com>
2021-03-27 23:08:57 +1100

    Populated first few tables with sample data.
    Also added an ALTER DATABASE statement at the start to allow ease of rebuilding during testing.

--- CreateAndPopulateDB.sql
+++ CreateAndPopulateDB.sql
@@ -14,6 +14,8 @@ Lukas Binninger c3332295
 -- create database
 USE master
 GO
+ALTER DATABASE COMP3320_A1_PizzaDB SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+GO
 
 IF EXISTS (SELECT * FROM sysdatabases WHERE name='COMP3320_A1_PizzaDB')
 BEGIN
@@ -279,78 +281,87 @@ GO
 
 
 -- 3.2 populate with sufficient sample data
-/*
+
 -- some sample so we have a format
 INSERT INTO DiscountProgram(discountCode, description, startDate, endDate, requirements, discountPercentage)
 VALUES
-	-- students
-	('Yuji', 'Ishikawa', '69', 'Mint Chocolate Road', 'Fukuoaka', 1504, '+81123123', 'kamikaze@battleship.jp', 'Loves Algorithms'),
-	('Liam', 'Craft', '3', 'Under the Bridge', 'Toronto', 1511, '+61123123', 'maurie@mole.com', 'Leader of Barbecue Society'),
-	('Ayami', 'Nonaka', '69', 'Mint Chocolate Road', 'Fukuoaka', 1504, '+81321321', 'friedchicken@gmail.jp', 'Leader of Japanese Club'),
-	-- staff
-	('Nathan', 'Gervasoni', '68', 'Rainbow Road', 'Callaghan', 1212, '+61555666', 'goleo@gmail.com', 'University Mascot'),
-	('Healthy', 'Harold', '2', 'Special Street', 'Mayfield', 2304, '+61333222', 'giraffe@gmail.com', 'Health Advisor'),
-	('Jeff', 'Jefferson', '1/1A', 'Jeff Drive', 'Jefftown', 3332, '+61444222', 'jeff.jefferson@gmail.com', 'Oldest lecturer at university')
+	('E2021', 'Easter weekend sale', '2021-03-02 08:00:00', '2021-03-04 21:59:59', 'none', 50.00),
+	('Thurs2021', 'Hungry Thursdays', '2021-01-01 08:00:00', '2021-12-31 21:59:59', 'Only on Thursdays!', 25.00),
+	('Sept2021', 'September memorial sale', '2021-09-11 08:00:00', '2021-09-11 21:59:59', 'none', 69.00),
+	('nov2021', 'Liams birthday sale', '2021-11-15 08:00:00', '2021-09-11 21:59:59', 'none', 42.00),
+	('oct2021', 'OctoberFest', '2021-10-01 08:00:00', '2021-10-08 21:59:59', 'In store customers only. Must greet server in Deutsche', 80.00)
 GO
 
 
-INSERT INTO Customer()
+INSERT INTO Customer(firstName, lastName, street, suburb, postcode)
 VALUES
-	--
-	()
+	('Cleetus', 'McFarlane', '123 Redneck St', 'Daytona', 5432),
+	('Phil', 'McCrackin', '89 First St', 'Boolaroo', 2284),
+	('Steph', 'Smith', '1 Hill Rd', 'Lambton', 2267),
+	('Jill', 'Qwerty', '222 School Rd', 'Newcastle', 1123),
+	('Fred', 'Fredson', '67 Test St', 'TestTown', 4444),
+	('Hayden', 'Milkson', '567 Latte rd', 'Ourimbah', 3211)
 GO
 
-INSERT INTO CustomerOnlineCustomer()
+INSERT INTO CustomerOnlineCustomer(customerID, email, password, phone)
 VALUES
-	--
-	()
+	(1, 'cleetus@email.com', 'samplePW', '0404123123'),
+	(2, 'legend@email.com', 'mydogsname', '(02)49753344'),
+	(6, 'Haydo@soybeans.com', 'noswearing', '0411009009'),
+	(5, 'dogecoin@email.com', 'toTheMoon', '0499321321')
 GO
 
-INSERT INTO CustomerGuest()
+INSERT INTO CustomerGuest(customerID, email, phone)
 VALUES
-	--
-	()
+	(3, 'steph@email.com', '0412345345')
 GO
 
-INSERT INTO CustomerPhoneCustomer()
+INSERT INTO CustomerPhoneCustomer(customerID, phone)
 VALUES
-	--
-	()
+	(4, '0404998877')
 GO
 
-INSERT INTO Bank()
+INSERT INTO Bank(bankCode, bankName)
 VALUES
-	--
-	()
+	('666334', 'Bank of Newcastle'),
+	('444123', 'Callghan Building Society')
 GO
 
-INSERT INTO Employee()
+--NOTE: default status is used for employee
+INSERT INTO Employee(firstName, lastName, street, suburb, postcode, contactNumber, taxFileNumber, bankCode, accountNumber, description)
 VALUES
-	--
-	()
+	('Yuji', 'Ishikawa', '25 Lambton Rd', 'Lambton', 3345, '0404998877', '1234567890', '666334', 111112300, 'The man from Japan'),
+	('Lukas', 'Binninger', '12 LearningLounge Rd', 'Auchmuty', 2243, '0452123123', '1234567666', '444123',111222300, 'Specialty: Schnitzel supreme pizza'),
+	('Liam', 'Craft', '86 SeydaNeen St', 'Morrowind', 1111, '0499000111', '1234567000', '666334', 111333300, 'Permanent mop duties'),
+	('Nathan', 'Gervasoni', '112 Mamamia Drive', 'MarioLand', 2222, '0455654654', '1234567111', '444123', 111444300, 'Actually Italian')
 GO
 
-INSERT INTO EmployeeInShopWorker()
+INSERT INTO EmployeeInShopWorker(empNumber, paymentRateHourly)
 VALUES
-	--
-	()
+	(1, 22.00),
+	(4, 22.00)
 GO
 
-INSERT INTO EmployeeDeliveryDriver()
+INSERT INTO EmployeeDeliveryDriver(empNumber, driversLicense, paymentRatePerDelivery)
 VALUES
-	--
-	()
+	(2, 555222, 5.00),
+	(3, 555888, 5.00)
 GO
 
-INSERT INTO Shift()
+INSERT INTO Shift(startDateTime, endDateTime, numOfDeliveries, paymentAmount, paymentSent, empNumber)
 VALUES
-	--
-	()
+	('2021-03-01 08:00:00','2021-03-01 20:00:00', 15, 75.00, 1, 2),
+	('2021-03-01 08:00:00','2021-03-01 20:00:00', 13, 65.00, 1, 3),
+	('2021-03-01 08:00:00','2021-03-01 20:00:00', null, 176.00, 1, 1),
+	('2021-03-01 08:00:00','2021-03-01 20:00:00', null, 176.00, 1, 4)
 GO
-
-INSERT INTO Order()
+/*
+INSERT INTO FoodOrder(orderDateTime, discountAmount, tax, totalAmountDue, status, description, fulfillmentDateTime, isDelivery,
+				orderType, paymentMethod, paymentApprovalNumber, discountCode, customerID, workerID, driverID)
 VALUES
-	--
+	(SYSDATETIME(), 00.00, 10.00, 100.00, DEFAULT, 'No nuts plz', SYSDATETIME(), 0, 'phone', 'cash', null, null, 4, null),
+	(SYSDATETIME(), 20.00, 10.00, 100.00, DEFAULT, 'Extra banana', SYSDATETIME(), 1, 'phone', 'cash', null, null, 1, null),
+	(),
 	()
 GO
 
