a3d0bdec7e7d74cbc28f21e3680e00db488a003d
Lukas <lukas.binninger@gmail.com>
2021-03-28 18:50:25 +1100

    finished SP createOrder
    haven't tested it yet

--- create_usp_ createCustomerOrder.sql
+++ create_usp_ createCustomerOrder.sql
@@ -39,6 +39,9 @@ GO
 DROP TYPE ItemsOrderedType
 GO
 
+DROP TYPE MenuItemIngredientType
+GO
+
 -- set up TVP for items ordered
 CREATE TYPE ItemsOrderedType AS TABLE
 (
@@ -46,6 +49,16 @@ CREATE TYPE ItemsOrderedType AS TABLE
 	quantityOrdered INT,
 
 	PRIMARY KEY (itemNumber, quantityOrdered)
+)
+GO
+
+-- set up TVP for list of menuItem - ingredients mapping
+CREATE TYPE MenuItemIngredientType AS TABLE
+(
+	ingrCode INT,
+	quantity INT,
+
+	PRIMARY KEY (ingrCode, quantity)
 )
 GO
 
@@ -151,13 +164,94 @@ BEGIN
 		DECLARE @orderID INT
 		SET @orderID =
 			(
-				SELECT	*
+				SELECT	orderID
 				FROM	FoodOrder
 				WHERE	customerID = @customerID
 					AND	orderDateTime = @orderDateTime
 			)
 
-		--
+		-- add mapping in OrderMenuItemRelation and update ingredient's suggestedCurrentStockLevel
+		-- step through list of items again
+		-- open and populate cursor
+		OPEN itemCursor
+
+		-- get first row
+		FETCH NEXT FROM itemCursor INTO @itemNumber, @quantityOrdered
+
+		-- set up list of ingredients associated with specific item
+		DECLARE @ingredientList MenuItemIngredientType
+
+		-- get item by item from cursor
+		-- while still more rows
+		WHILE @@FETCH_STATUS = 0
+		BEGIN
+			-- calculate subtotal for that item
+			DECLARE @subtotal SMALLMONEY
+
+			SET @itemPrice =
+					(
+						SELECT	price
+						FROM	MenuItem
+						WHERE	itemCode = @itemNumber
+					)
+			SET @subtotal = @itemPrice * @quantityOrdered
+
+			-- insert into table for each item
+			INSERT INTO OrderMenuItemRelation(orderID, itemCode, quantity, subtotal)
+			VALUES	(@orderID, @itemNumber, @quantityOrdered, @subtotal)
+
+			-- also for each item, check which ingredients are needed
+			-- get all the ingredients mapped to that ordered menu item
+			INSERT INTO @ingredientList
+					SELECT	ingrCode, quantity
+					FROM	MenuItemIngredientRelation
+					WHERE	itemCode = @itemNumber
+
+			-- declare cursor to access rows of ingredients of a menuitem one by one
+			DECLARE ingredientCursor CURSOR
+			FOR
+				SELECT	*
+				FROM	@ingredientList
+			FOR READ ONLY
+
+				-- declare variables to fetch individual rows
+				DECLARE @ingrCode INT
+				DECLARE @ingrQuantity INT
+
+				-- open and populate cursor
+				OPEN ingredientCursor
+
+				-- get first row
+				FETCH NEXT FROM ingredientCursor INTO @ingrCode, @ingrQuantity
+
+				-- get ingredient by ingredient from cursor
+				-- while still more rows
+				WHILE @@FETCH_STATUS = 0
+				BEGIN
+					-- calculate needed number of ingredients
+					DECLARE @stockDecrease INT
+					SET @stockDecrease = @ingrQuantity * @quantityOrdered
+
+					-- update ingredient's suggestedCurrentStockLevel
+					UPDATE	Ingredient
+					SET		suggestedCurrentStockLevel = suggestedCurrentStockLevel - @stockDecrease
+
+					-- fetch next row
+					FETCH NEXT FROM ingredientCursor INTO @ingrCode, @ingrQuantity
+				END
+
+				-- close cursor
+				CLOSE ingredientCursor
+
+			-- removes the cursor reference
+			DEALLOCATE ingredientCursor
+
+			-- fetch next row
+			FETCH NEXT FROM itemCursor INTO @itemNumber, @quantityOrdered
+		END
+
+		-- close cursor
+		CLOSE itemCursor
 
 
 	END TRY
@@ -169,7 +263,7 @@ BEGIN
 
 
 	-- removes the cursor reference
-			DEALLOCATE itemCursor
+	DEALLOCATE itemCursor
 END
 GO
 
