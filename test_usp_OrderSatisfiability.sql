d07692f77f862ae6ccfda9cfd93a9b43d4c4e9db
Lukas <lukas.binninger@gmail.com>
2021-03-29 00:33:10 +1100

    some submission formalities

---
+++ test_usp_OrderSatisfiability.sql
@@ -0,0 +1,7 @@
+-- NOTE
+/*
+We read that we have to include this file to test the order satisfiability SP, but as we already called this SP
+in the create order SP, we've already tested it in there.
+
+Our test data for both cases is therefore in the file test_usp_createCustomerOrder_17.sql
+*/
